b55c9952f92e244290211be2ff3cef4b4a47af38
Regina Campos <rmscampos@gmail.com>
2019-11-27 14:06:17 -0600

    completes all flights labs

--- controllers/flights.js
+++ controllers/flights.js
@@ -11,7 +11,7 @@ module.exports = {
 function index(req, res) {
     Flight.find({}, function(err, flights) {
         res.render('flights/index', { title: 'All Flights', flights});
-    });
+    }).sort('departs');
 }
 
 function show(req, res) {
